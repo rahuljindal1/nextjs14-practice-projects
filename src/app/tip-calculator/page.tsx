4f3ea9ce5517d5e96409188513a16666b0301032
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-07-06 11:02:02 +0530

    chore: Move components to components folder for tip-calculator

--- src/app/tip-calculator/page.tsx
+++ src/app/tip-calculator/page.tsx
@@ -1,4 +1,4 @@
-import TipCalculatorCard from "./TipCard";
+import TipCalculatorCard from "./components/TipCard";
 import classes from "./styles.module.css";
 
 export default function TipCalculator() {
