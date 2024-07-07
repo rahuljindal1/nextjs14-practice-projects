586081c5ff000fff9bb212b3314a26c4ea61a991
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-07-07 22:32:02 +0530

    feat: Implement Rock Paper Scissors game

---
+++ src/app/rock-paper-scissors/components/Card/index.tsx
@@ -0,0 +1,53 @@
+import classNames from "classnames";
+import styles from "./styles.module.css";
+
+export enum CardTypeEnum {
+  scissors = "scissors",
+  rock = "rock",
+  paper = "paper"
+}
+
+const GAME_ITEMS: {
+  id: CardTypeEnum;
+  symbol: string;
+  customStyles: string[];
+}[] = [
+  {
+    id: CardTypeEnum.scissors,
+    symbol: "✂️",
+    customStyles: [styles.scissors]
+  },
+  {
+    id: CardTypeEnum.rock,
+    symbol: "🪨",
+    customStyles: [styles.rock]
+  },
+  {
+    id: CardTypeEnum.paper,
+    symbol: "🖐🏻",
+    customStyles: [styles.paper]
+  }
+];
+
+export default function Card({
+  cardType,
+  onClick
+}: {
+  cardType: CardTypeEnum;
+  onClick: (cardType: CardTypeEnum) => void;
+}) {
+  const gameItem = GAME_ITEMS.find((item) => item.id === cardType);
+
+  if (!gameItem) {
+    return <></>;
+  }
+
+  return (
+    <div
+      className={classNames(styles.mainContainer, gameItem.customStyles)}
+      onClick={() => onClick(gameItem.id)}
+    >
+      {gameItem.symbol}
+    </div>
+  );
+}
