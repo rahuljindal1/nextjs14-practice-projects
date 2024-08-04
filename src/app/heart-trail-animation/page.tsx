42b1a745a39fe8fe7e953955bd4f3dfb41e7dca5
Rahul Jindal <rahul.jindaliit@gmail.com>
2024-08-04 17:31:31 +0530

    chore: Add heart trail animation

--- src/app/heart-trail-animation/page.tsx
+++ src/app/heart-trail-animation/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import styles from "./styles.module.css";
+
 export default function HeartTrailAnimation() {
-  return <div>Heart Trail Animation</div>;
+  const mouseMoveHandler = (e: any) => {
+    const parent = document.getElementById("heart");
+
+    const xPos = e.clientX;
+    const yPos = e.clientY;
+    const spanEl = document.createElement("span");
+    spanEl.classList.add(styles.heart);
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+    parent?.appendChild(spanEl);
+    setTimeout(() => {
+      spanEl.remove();
+    }, 3000);
+  };
+
+  return (
+    <div
+      id="heart"
+      onMouseMove={mouseMoveHandler}
+      className={styles.mainContainer}
+    ></div>
+  );
 }
